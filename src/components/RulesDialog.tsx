e00b04806a87d4ce301ed47a4d8e9c9ec0e8f607
tillganster <tillganster@web.de>
2025-02-15 13:24:17 +0100

    feat: store score in localstorage

--- src/components/RulesDialog.tsx
+++ src/components/RulesDialog.tsx
@@ -5,11 +5,11 @@ export default function RulesDialog() {
   //@ts-ignore
   return (
     <div
-      class={`${!store.showRules && "hidden"} h-screen overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center flex items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full`}
+      class={`${!store.showRules && "translate-y-full scale-50 opacity-0"} bg-black/20 translate-0 opacity-100  transition-transform duration-700 h-screen overflow-y-auto overflow-x-hidden fixed top-0 right-0 z-50 justify-center flex items-center w-full md:inset-0  max-h-full`}
     >
       <div
         class={
-          "w-full my-0 mx-auto bg-white h-full flex flex-col py-10 justify-center items-center"
+          "md:w-1/2 md:h-1/2 md:rounded-xl w-full my-0 mx-auto bg-white h-full flex flex-col py-10 justify-center items-center"
         }
       >
         <h1
@@ -18,14 +18,14 @@ export default function RulesDialog() {
           Rules
         </h1>
         <div class={"flex justify-center items-center grow-1"}>
-          <img src={"/images/image-rules.svg"} />
+          <img src={"./images/image-rules.svg"} />
         </div>
 
         <button
           onClick={() => setStore("showRules", false)}
-          class={"items-center cursor-pointer active:scale-150"}
+          class={" items-center cursor-pointer active:scale-150"}
         >
-          <img src={"/images/icon-close.svg"} />
+          <img src={"./images/icon-close.svg"} />
         </button>
       </div>
     </div>
--- src/stores/game.store.ts
+++ src/stores/game.store.ts
@@ -1,5 +1,6 @@
 import { createStore } from "solid-js/store";
 import { PickOptions } from "~/model/picks.model";
+import { makePersisted } from "@solid-primitives/storage";
 
 interface Store {
   score: number;
@@ -7,8 +8,14 @@ interface Store {
   showRules: boolean;
 }
 
-export const [store, setStore] = createStore<Store>({
-  score: 0,
+const SCORE_KEY = "game.score";
+const savedScore = localStorage.getItem(SCORE_KEY);
+export const [store, _setStore] = createStore<Store>({
+  score: Number(savedScore) ?? 0,
   userPick: undefined,
   showRules: false,
 });
+export const setStore = (key: keyof Store, val: any) => {
+  _setStore(key, val);
+  if (key === "score") localStorage.setItem(SCORE_KEY, `${store.score}`);
+};
