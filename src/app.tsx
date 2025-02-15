676018a84422bc5bd80217e88f23994eda60556a
tillganster <tillganster@web.de>
2025-02-15 13:33:18 +0100

    fix: favicon

--- src/app.tsx
+++ src/app.tsx
@@ -5,6 +5,7 @@ import { Suspense } from "solid-js";
 import "./app.css";
 import Scoreboard from "~/components/Scoreboard";
 import RulesButton from "~/components/Rules.button";
+import { Link, MetaProvider } from "@solidjs/meta";
 
 export default function App() {
   return (
@@ -12,6 +13,13 @@ export default function App() {
       base={import.meta.env.SERVER_BASE_URL}
       root={(props) => (
         <>
+          <MetaProvider>
+            <Link
+              rel="mask-icon"
+              color={"white"}
+              href={`${import.meta.env.SERVER_BASE_URL}logo.svg`}
+            />
+          </MetaProvider>
           <main
             style={{ height: "100dvh" }}
             class=" h-screen w-screen flex flex-col justify-around p-10"
